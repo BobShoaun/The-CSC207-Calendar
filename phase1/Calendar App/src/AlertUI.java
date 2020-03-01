603ff0146fed69a63b0d073dad9da567034a88a2
devliegh <colin.de@mail.utoronto.ca>
2020-03-01 18:42:08 -0500

    Started AlertUI

--- phase1/Calendar App/src/AlertUI.java
+++ phase1/Calendar App/src/AlertUI.java
@@ -1,2 +1,14 @@
+//**********************************************
+//  AlertUI.java
+//**********************************************
+import java.util.Scanner;
+
 public class AlertUI {
+
+    private AlertCollection alerts;
+
+    public AlertUI(AlertCollection ac) {
+        alerts = ac;
+    }
+
 }
