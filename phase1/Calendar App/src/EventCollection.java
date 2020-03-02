1d303b432ee4364569732d0ea89ae05694fb7178
huan1850 <richardxr.jhuang@mail.utoronto.ca>
2020-03-01 23:03:33 -0500

    implemented some functions

--- phase1/Calendar App/src/EventCollection.java
+++ phase1/Calendar App/src/EventCollection.java
@@ -10,13 +10,52 @@ import java.util.List;
 public class EventCollection implements Serializable
 {
     private ArrayList<Event> events = new ArrayList<>();
-    private String name = null;
+    private String name;
+
+    /**
+     * constructor for list of regular events
+     * @param events list of regular events
+     */
+    public EventCollection(ArrayList<Event> events)
+    {
+        this.events = events;
+    }
+
+    /**
+     * constructor for a series
+     * @param name name of series
+     * @param events list of events of the series
+     */
+    public EventCollection(String name, ArrayList<Event> events)
+    {
+        this.name = name;
+        this.events = events;
+    }
 
     public String getName(){return name;}
 
     public Event getEvent(String id){ throw new NotImplementedException(); };
-    public List<Event> getEvents(Date date) {throw new NotImplementedException(); }
+
+    /**
+     * Returns all events that is
+     * @param date date of events demanded
+     * @return a list of events that is on the same day as <date></>
+     */
+    public List<Event> getEvents(Date date)
+    {
+        List<Event> ret = new ArrayList<>();
+        for (Event e:this.events)
+        {
+            //omitted for now
+        }
+        return ret;
+    }
     public List<Event> getEvents(Date start, Date end) {throw new NotImplementedException();}
     public Iterator<Event> getEventIterator(Date start) {throw new NotImplementedException();}
-    public void addEvent(Event event){throw new NotImplementedException();}
+
+    /**
+     * add an event to this list
+     * @param event the event to be added
+     */
+    public void addEvent(Event event){this.events.add(event);}
 }
