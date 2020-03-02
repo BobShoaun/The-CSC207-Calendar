2705138fd1f631a7f04a3c136f5d5df8f8a5cd60
chupengc <deer10228@gmail.com>
2020-03-02 15:57:22 -0500

    implemented functions in event

--- phase1/Calendar App/src/Event.java
+++ phase1/Calendar App/src/Event.java
@@ -1,9 +1,12 @@
+import javax.lang.model.element.NestingKind;
 import java.util.Date;
+import java.util.Observable;
+import java.util.Observer;
 
 /**
  * Event class
  */
-public class Event {
+public class Event implements Observer{
 
     private String id;
     private String name;
@@ -11,6 +14,13 @@ public class Event {
     private Date startDate;
     private Date endDate;
 
+    /**
+     * Constructor for a new Event
+     * @param id id of the Event
+     * @param name name of the Event
+     * @param startDate start time of the Event
+     * @param endDate end time of the Event
+     */
     public Event (String id, String name, Date startDate, Date endDate) {
         this.id = id;
         this.name = name;
@@ -18,9 +28,52 @@ public class Event {
         this.endDate = endDate;
     }
 
+    /**
+     * Return the name of the Event
+     * @return the name of the Event
+     */
     public String getName () {
         return name;
     }
 
+    /**
+     * Return the start time of the Event
+     * @return the startDate of the Event
+     */
     public Date getStartDate() {return startDate; }
+
+    /**
+     * Set the name of the Event
+     * @param newName the new name of the Event
+     */
+    public void setName(String newName) { this.name = newName; }
+
+    /**
+     * Set the description of the Event
+     * @param d the Event description
+     */
+    public void setDescription(String d) { this.description = d; }
+
+    /**
+     * Set the start time of the Event
+     * @param newStart the new startDate of the Event
+     */
+    public void setStartDate(Date newStart) { this.startDate = newStart; }
+
+    /**
+     * Set the end time of the Event
+     * @param newEnd the new endDate of the Event
+     */
+    public void setEndDate(Date newEnd) { this.endDate = newEnd; }
+
+    /**
+     * Return the duration of the Event in Date
+     * @return duration of the Event
+     */
+    public Date getDuration() {
+        long dur = startDate.getTime() - endDate.getTime();
+        Date durDate = new Date(dur);
+    }
+
+
 }
