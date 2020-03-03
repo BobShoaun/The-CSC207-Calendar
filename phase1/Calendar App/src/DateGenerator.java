9a35c9d051c5e4afc6458763f899a89d498d5844
devliegh <colin.de@mail.utoronto.ca>
2020-03-02 22:43:21 -0500

    Created ignoreList public interface in DateGenerator

--- phase1/Calendar App/src/DateGenerator.java
+++ phase1/Calendar App/src/DateGenerator.java
@@ -1,8 +1,12 @@
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
+import java.util.List;
 
 public class DateGenerator implements Iterable<Date>{
+
+    private List<Date> ignoreList = new ArrayList<>();
 
     // end==null means forever? up to implementation
     public DateGenerator(Date start, Duration period, Date end) {
@@ -12,5 +16,9 @@ public class DateGenerator implements Iterable<Date>{
     @Override
     public Iterator<Date> iterator() {
         return null;
+    }
+
+    public List<Date> getIgnoreList() {
+        return ignoreList;
     }
 }
