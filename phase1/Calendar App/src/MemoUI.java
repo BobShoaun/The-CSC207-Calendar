d262918299a94316160275bb5ee1547df41162ba
OrangeTuxedoNinja <sachagoldman@icloud.com>
2020-03-05 16:03:57 -0500

    Finished setting up memo UI.

--- phase1/Calendar App/src/MemoUI.java
+++ phase1/Calendar App/src/MemoUI.java
@@ -2,14 +2,15 @@ import java.io.IOException;
 
 public class MemoUI extends UserInterface {
     Memo memo;
+    Calendar calendar;
 
-    public MemoUI(Memo memo){
+    public MemoUI(Memo memo, Calendar calendar){
         this.memo = memo;
     }
 
     @Override
     public void display(){
-         System.out.println(memo.getText() + "\n\n" + memo.getText());
+         System.out.println(memo.getTitle() + "\n\n" + memo.getText());
     }
 
     @Override
@@ -19,13 +20,16 @@ public class MemoUI extends UserInterface {
             int option = getOptionsInput(new String[]{"Edit Title", "Edit Text", "Delete", "Exit"});
             switch (option) {
                 case 1:
+                    memo.setTitle(this.getStringInput("Enter new title: "));
 
                 case 2:
+                    memo.setText(this.getStringInput("Enter new text: "));
 
                 case 3:
+                    calendar.removeMemo(memo);
 
                 case 4:
-
+                    running = false;
             }
         }
     }
