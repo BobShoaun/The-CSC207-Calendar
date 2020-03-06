c541c8a2fa0e777500eabc0430b3fe38a6bfad3d
chupengc <deer10228@gmail.com>
2020-03-05 19:06:48 -0500

    Modified show memos

--- phase1/Calendar App/src/EventUI.java
+++ phase1/Calendar App/src/EventUI.java
@@ -1,7 +1,10 @@
+import exceptions.InvalidDateException;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,20 +35,46 @@ public class EventUI extends UserInterface {
     public void show() {
         boolean running = true;
         while (running) {
-            int option = getOptionsInput(new String[]{"Show Memos", "Show Tags", "Edit Memos", "Edit Tags"});
+            int option = getOptionsInput(new String[]{"Exit", "Show Duration" ,"Edit Event", "Show Alerts", "Edit Alert", "Show Memos", "Edit Memos", "Show Tags", "Edit Tags"});
             switch (option) {
+                case 0:
+                    running = false;
+                    break;
                 case 1:
+                    System.out.println("This event lasts for ");
+                    break;
+                case 2:
+                    editEvent();
+                    break;
+                case 3:
+                    List<AlertCollection> alertCollections = calendar.alertCollections;
+                    for (AlertCollection ac: alertCollections) {
+                        if (ac.getEventId().equals(event.getId())) {
+                            System.out.println(ac.toString());
+                        }
+                    }
+                    break;
+                case 4:
+                    break;
+                case 5:
                     List<Memo> memos = calendar.getMemos();
+                    StringBuilder result = new StringBuilder();
                     for (Memo m: memos) {
                         int i = 0;
                         if ( m.hasEvent(event.getId()) ) {
                             String num = Integer.toString(i);
+                            result.append("[" + num + "]" + m.getTitle() + "\n" + m.getText() + "\n\n");
                             i += 1;
-                            System.out.println(num + ". " + m.getTitle() + "\n" + m.getText() + "\n\n");
                         }
                     }
+                    System.out.println(result);
                     break;
-                case 2:
+                case 6:
+                    int num = getIntInput("Memo no.: ", 0, memoUIs.size() - 1);
+                    MemoUI mui = memoUIs.get(num);
+                    mui.show();
+                    break;
+                case 7:
                     List<Tag> tags = calendar.tags;
                     for (Tag t: tags) {
                         if ( t.hasEvent(event.getId()) ) {
@@ -53,14 +82,41 @@ public class EventUI extends UserInterface {
                         }
                     }
                     break;
-                case 3:
-                    int num = getIntInput("Memo no.: ", 0, memoUIs.size() - 1);
-                    MemoUI mui = memoUIs.get(num);
-                    mui.show();
-                    break;
-                case 4:
+                case 8:
                     editTag();
                     break;
+            }
+        }
+    }
+
+    private void editEvent() {
+        boolean editing = true;
+        while (editing) {
+            int options = getOptionsInput(new String[]{"Exit", "Change name", "Change start time", "Change end time"});
+            switch (options) {
+                case 0:
+                    editing = false;
+                    break;
+                case 1:
+                    String name = getStringInput("Enter new event name: ");
+                    event.setName(name);
+                    break;
+                case 2:
+                    try {
+                        GregorianCalendar start = getDateInput("Enter new start time: ");
+                        event.setStartDate(start);
+                    } catch (InvalidDateException e) {
+                        System.out.println("Invalid time input. Start time cannot be after current end time.");
+                        editEvent();
+                    }
+                case 3:
+                    try {
+                        GregorianCalendar end = getDateInput("Enter new end time: ");
+                        event.setEndDate(end);
+                    } catch (InvalidDateException e) {
+                        System.out.println("Invalid time input. End time cannot be before current start time.");
+                        editEvent();
+                    }
             }
         }
     }
@@ -73,10 +129,10 @@ public class EventUI extends UserInterface {
 
     private void editTag() {
         List<Tag> tags = calendar.tags;
-        String tagName = getStringInput("Tag name: ");
+        String tagName = getStringInput("Enter tag name: ");
         for (Tag t: tags) {
             if ( t.getText().equals(tagName) )  {
-                String newText = getStringInput("New tag text: ");
+                String newText = getStringInput("Enter new tag text: ");
                 t.setText(newText);
             }
         }
