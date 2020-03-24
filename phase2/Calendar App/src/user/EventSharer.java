db0b028bcb39c66122ad7ca63a3feabceb79e62a
OrangeTuxedoNinja <sachagoldman@icloud.com>
2020-03-24 18:35:27 -0400

    Added documenetation for EventSharer

--- phase2/Calendar App/src/user/EventSharer.java
+++ phase2/Calendar App/src/user/EventSharer.java
@@ -6,13 +6,26 @@ import exceptions.InvalidUsernameException;
 import java.io.IOException;
 
 
+/**
+ * EventSharer class
+ */
 public class EventSharer {
     private UserManager userManager;
 
+    /**
+     * Constructor for creating an EventSharer
+     */
     public EventSharer(UserManager userManager){
         this.userManager = userManager;
     }
 
+    /**
+     * @param event the event being shared
+     * @param recipientName the username of the user who the event is being shared with
+     * @param recipientCalName the calendar name of the user with who the event is being shared calendar in which the event is added
+     * @throws InvalidUsernameException if the username passed is invalid
+     * @throws InvalidCalendarNameException if the username calendarnName is invalid
+     */
     public void share(Event event, String recipientName, String recipientCalName) throws InvalidUsernameException, InvalidCalendarNameException {
         User recipientUser = userManager.getUser(recipientName);
         if (recipientUser == null){
