8861229b5b6f847fda9ac65fc20d02391a3f9d2a
chupengc <deer10228@gmail.com>
2020-03-26 21:03:38 -0400

    added EventController

---
+++ phase2/Calendar App/src/gui/EventController.java
@@ -0,0 +1,22 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class EventController {
+
+    @FXML
+    private TextField eventName;
+
+    @FXML
+    private Button done;
+
+    @FXML
+    private Button cancel;
+
+    @FXML
+    private Button alerts;
+
+    public EventController() {};
+
+
+}
