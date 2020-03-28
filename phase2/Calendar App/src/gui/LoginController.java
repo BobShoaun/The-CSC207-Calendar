919f32f71dc2192c9f82a30fb70cbb84698c5496
devliegh <colin.de@mail.utoronto.ca>
2020-03-27 22:09:52 -0400

    I made a super epic login controller but stuff doesn't compile :(

--- phase2/Calendar App/src/gui/LoginController.java
+++ phase2/Calendar App/src/gui/LoginController.java
@@ -1,4 +1,46 @@
 package gui;
 
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.stage.Stage;
+import user.UserManager;
+
+import java.awt.*;
+import java.io.IOException;
+
 public class LoginController {
+
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Label bottomMessage;
+
+    private UserManager userManager = new UserManager();
+
+    @FXML
+    private void handleLogin(Event e) throws IOException {
+        String usernameText = username.getText();
+        String passwordText = password.getText();
+
+        if (!userManager.loginUser(usernameText, passwordText)) {
+            bottomMessage.setText("Sorry, that didn't work. Please try again.");
+            bottomMessage.setFont(new Font("Source Code Pro", Font.PLAIN, 16));
+            bottomMessage.setBackground(Color.RED);
+        } else {
+            Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+            stage.hide();
+            Parent root = FXMLLoader.load(getClass().getResource("calendar.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+    }
+
 }
--- phase2/Calendar App/src/gui/Main.java
+++ phase2/Calendar App/src/gui/Main.java
@@ -11,11 +11,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
-        primaryStage.setTitle("Calendar");
-        primaryStage.setScene(new Scene(root, 400, 250));
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
