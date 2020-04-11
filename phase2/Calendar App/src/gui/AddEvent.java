389055e0b417bf205e3657aadc1f558ed0f5b6ca
Ng Bob Shoaun <ngbobshoaun2000@gmail.com>
2020-04-11 16:08:18 -0400

    add Event UI

---
+++ phase2/Calendar App/src/gui/AddEvent.java
@@ -0,0 +1,33 @@
+package gui;
+
+import exceptions.InvalidDateException;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
+public class AddEvent {
+
+    @FXML
+    private TextField nameField;
+    @FXML
+    private DatePicker startTime;
+    @FXML
+    private DatePicker endTime;
+    @FXML
+    private TextField seriesField;
+    @FXML
+    private TextField tagsField;
+    @FXML
+    private TextField memosField;
+
+    @FXML
+    private void handleDone(Event e) throws IOException, InvalidDateException {
+
+    }
+
+}
