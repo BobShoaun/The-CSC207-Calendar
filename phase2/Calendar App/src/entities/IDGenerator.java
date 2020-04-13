bf77028025a940300bb51837f3ba7c079fe6223c
huan1850 <richardxr.jhuang@mail.utoronto.ca>
2020-04-13 12:44:23 -0400

    addEvent.fxml controller pushed

--- phase2/Calendar App/src/entities/IDGenerator.java
+++ phase2/Calendar App/src/entities/IDGenerator.java
@@ -5,6 +5,7 @@ import java.util.GregorianCalendar;
 public class IDGenerator {
     public static String generateEventId(String eventName, GregorianCalendar eventStartTime) {
         String id = eventStartTime.toString() + "%" + eventName;
+        System.out.println("ID:"+id);
         return id.replaceAll(" ", "%");
     }
 }
