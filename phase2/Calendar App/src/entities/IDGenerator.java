ae03cb88a0c61dfeba3394863dd493dda86e2277
devliegh <colin.de@mail.utoronto.ca>
2020-04-13 12:49:46 -0400

    fix print statement

--- phase2/Calendar App/src/entities/IDGenerator.java
+++ phase2/Calendar App/src/entities/IDGenerator.java
@@ -5,7 +5,8 @@ import java.util.GregorianCalendar;
 public class IDGenerator {
     public static String generateEventId(String eventName, GregorianCalendar eventStartTime) {
         String id = eventStartTime.getTime().toString() + "%" + eventName;
-        System.out.println("ID:"+id);
-        return id.replaceAll(" ", "%");
+        id = id.replaceAll(" ", "%");
+        System.out.println("ID:" + id);
+        return id;
     }
 }
