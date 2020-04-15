d44d903dcf588730b3337c81932c224a42215686
devliegh <colin.de@mail.utoronto.ca>
2020-04-14 21:13:12 -0400

    add Alert functionality

--- phase2/Calendar App/src/gui/Alert.java
+++ phase2/Calendar App/src/gui/Alert.java
@@ -1,9 +1,11 @@
 package gui;
 
+import com.sun.istack.internal.NotNull;
 import entities.AlertCollection;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import user.DataSaver;
 
@@ -14,29 +16,47 @@ import java.util.stream.Collectors;
 import static entities.IDManager.parseEventId;
 
 public class Alert extends GraphicalUserInterface {
-    private AlertCollection ac;
+    private final AlertCollection ac;
     @FXML
     ListView<String> manAlertList;
     @FXML
     ListView<String> repAlertList;
+    @FXML
+    Label title;
     private String currManAlert;
     private String currRepAlert;
 
-    protected void setAlertCollection(AlertCollection ac) {
+    /**
+     * Initialize this controller
+     *
+     * @param ac The AlertCollection
+     */
+    public Alert(@NotNull AlertCollection ac) {
         this.ac = ac;
+        title.setText("Alerts for " + ac.getEventId());
+        updateRepeatingAlerts();
+        updateManAlerts();
     }
 
+    /**
+     * Update the manual alerts list.
+     */
     private void updateManAlerts() {
         if (ac == null)
             System.out.println("AC has not been set!");
+        assert ac != null;
         ObservableList<String> alertStrings = FXCollections.observableArrayList(
                 ac.getManAlerts().stream().map(entities.Alert::toString).collect(Collectors.toList()));
         manAlertList.setItems(alertStrings);
     }
 
+    /**
+     * Update the repeating alerts list.
+     */
     private void updateRepeatingAlerts() {
         if (ac == null)
             System.out.println("AC has not been set!");
+        assert ac != null;
         ObservableList<String> alertStrings = FXCollections.observableArrayList(
                 ac.getGeneratedAlerts(new GregorianCalendar(1950, Calendar.JANUARY, 0),
                         new GregorianCalendar(2100, Calendar.JANUARY, 0))
@@ -57,7 +77,7 @@ public class Alert extends GraphicalUserInterface {
     }
 
     @FXML
-    private void deleteManAlert() {
+    private void deleteManualAlert() {
         System.out.println("Clicked delete manual alert");
         if (currManAlert != null) {
             DataSaver ds = new DataSaver("");
@@ -92,6 +112,25 @@ public class Alert extends GraphicalUserInterface {
     private void addRepeatingAlert() {
         System.out.println("Clicked add repeating alert");
         openGUI("repeatingAlert.fxml");
+    }
+
+    @FXML
+    private void editManualAlert() {
+        System.out.println("Clicked edit manual alert");
+        if (currManAlert != null) {
+            ManualAlert controller = openGUI("manualAlert.fxml");
+            controller.setDate(parseEventId(currManAlert));
+        }
+    }
+
+    @FXML
+    private void editRepeatingAlert() {
+        System.out.println("Clicked edit repeating alert");
+        if (currRepAlert != null) {
+            RepeatingAlert controller = openGUI("repeatingAlert.fxml");
+            controller.setDate(parseEventId(currRepAlert));
+            controller.setPeriod(null); //TODO: fix
+        }
     }
 
 }
--- phase2/Calendar App/src/gui/ManualAlert.java
+++ phase2/Calendar App/src/gui/ManualAlert.java
@@ -1,4 +1,23 @@
 package gui;
 
-public class ManualAlert {
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+import java.time.LocalDate;
+import java.util.GregorianCalendar;
+
+public class ManualAlert extends GraphicalUserInterface {
+
+    @FXML
+    private DatePicker datePicker;
+    @FXML
+    private TextField textField;
+
+    protected void setDate(GregorianCalendar date) {
+        LocalDate localDate = LocalDate.of(date.get(GregorianCalendar.YEAR), date.get(GregorianCalendar.MONTH),
+                date.get(GregorianCalendar.DATE));
+        datePicker.setValue(localDate);
+        textField.setText("" + date.get(GregorianCalendar.HOUR) + date.get(GregorianCalendar.MINUTE));
+    }
 }
