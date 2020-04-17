9bb6b1a3c39e5d3194c4fa85837dabdb25d4bc39
huan1850 <richardxr.jhuang@mail.utoronto.ca>
2020-04-16 20:01:01 -0400

    created Series UI

---
+++ phase2/Calendar App/src/gui/SeriesUI.java
@@ -0,0 +1,38 @@
+package gui;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SeriesUI extends GraphicalUserInterface implements Initializable {
+
+    private final ObservableList<String> timeChoice = FXCollections.observableArrayList("Day","Week","Month","Year");
+    @FXML
+    public DatePicker startDate;
+    @FXML
+    public DatePicker endDate;
+    @FXML
+    public ChoiceBox timeChoiceBox;
+    @FXML
+    public TextField repeatNumField;
+    @FXML
+    public TextField nameField;
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        timeChoiceBox.setValue("Day");
+        timeChoiceBox.setItems(timeChoice);
+    }
+    public void handleAddSeries(){
+
+    }
+}
